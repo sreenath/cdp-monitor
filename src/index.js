2055fe08f2a30bb6513d7903953fd349e710211e
Sreenath T V <sree.harishree@gmail.com>
2018-07-07 20:44:59 +1000

    Open CDP external page functionality added

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
 require('isomorphic-fetch');
+const shell = require('electron').shell;
 
 var currentCup;
 
 const updateCup = document.getElementById('updateCup');
+const openCDPPageElement = document.getElementById('openCDP');
 
 updateCup.addEventListener('click', function(event) {
   currentCup= document.getElementById('cupID').value;
   getData();
 })
 
+openCDPPageElement.addEventListener('click', function() {
+  currentCup= document.getElementById('cupID').value;
+  let CDPLink = `https://mkr.tools/cdp/${currentCup}`;
+  shell.openExternal(CDPLink);
+})
+
 function getData() {
   if (currentCup != undefined) {
-    fetch("http://graphql.makerdao.com/v1", {
+    console.log(currentCup);
+    fetch('https://graphql.makerdao.com/v1', {
       method: 'POST',
-      headers: { 
-        'Content-Type': 'application/graphql'
-     },
-      body: JSON.stringify({ query: `{
-        getCup(id: 2442) {
-          id
-          pip
-          ratio
-        }
-      }` }),
+      headers: { 'Content-Type': 'application/graphql' },
+      body: JSON.stringify({ query: 'query { getCup(id: 2442) { id } }' }),
     })
     .then(err => document.getElementById('cup-id').innerHTML=err)
     .then(
